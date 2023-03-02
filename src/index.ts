bb772ecd30cd21f5b775630ba07531988c31659e
LiamSmith1992 <liamj.smith@hotmail.com>
2023-03-02 14:26:58 -0700

    enum

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,18 @@ let level;
 function render(document: string) {
   console.log(document);
 }
+
+let numbers: number[] = [];
+numbers.forEach(n => n.toExponential)
+
+
+// tuple set requirements for this variable for what is inside it's array
+let user: [number, string] = [1, 'yo']
+
+
+
+// enums 
+// enum Size { Small = 's' , Medium = 'm', Large = 'l' };
+enum Size { Small = 1, Medium, Large };
+let mySize: Size = Size.Medium;
+console.log(mySize);
